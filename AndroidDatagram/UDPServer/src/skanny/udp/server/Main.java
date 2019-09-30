664abdfad0ce2bb6094d33a3d21b0dabf3b303f2
Skanny <morandi@adesso.de>
2019-09-30 22:49:14 +0200

    repair

--- AndroidDatagram/UDPServer/src/skanny/udp/server/Main.java
+++ AndroidDatagram/UDPServer/src/skanny/udp/server/Main.java
@@ -3,7 +3,7 @@ package skanny.udp.server;
 public class Main {
 
     public static void main(String[] args) {
-        int port = 50001;
+        int port = 50002;
         // UdpUnicastServer server = new UdpUnicastServer(port);
         UdpUnicastClient client = new UdpUnicastClient(port);
         
